40e42ef7ea35bea1b52985134b8cc2edafbc724d
Agnieszka Chmielowiec <agnieszka.chmielowiec@currenda.pl>
2019-07-20 17:21:36 +0200

    Working game

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,4 +1,82 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Game {
+
+    private String movie;
+    private String goodLetter;
+    private String wrongLetter;
+
+    public Game(String movie) {
+        this.movie = movie;
+        this.goodLetter = "";
+        this.wrongLetter = "";
+    }
+
+    public void StartGame(int checkNumber) {
+
+        boolean isWin = false;
+        int numberOfCheck = 0;
+        while (numberOfCheck < checkNumber) {
+            System.out.println("You are guessing: " + changeLetterToUnderscores());
+
+            int numberOfWrongLetters = wrongLetter.isEmpty() ? 0 : wrongLetter.split(",").length;
+
+            System.out.println("You have guess " + numberOfWrongLetters + " wrong letters: " + wrongLetter);
+            Scanner scanner = new Scanner(System.in);
+            String letter = scanner.nextLine();
+
+            String letterToGuess = Character.toString(letter.charAt(0));
+
+            if (movie.indexOf(letterToGuess) > -1) {
+                goodLetter += letterToGuess;
+            } else if (wrongLetter.indexOf(letterToGuess) == -1) {
+                wrongLetter += (wrongLetter.isEmpty() ? "" : ",") + letterToGuess;
+                numberOfCheck++;
+            }
+
+            if (movie.equals(changeLetterToUnderscores())) {
+                System.out.println("YOU WIN! Movie title: " + movie);
+                isWin = true;
+                break;
+            }
+        }
+        if (!isWin) {
+            System.out.println("YOU LOSE! You tried guess to " + movie);
+        }
+
+
+    }
+
+    public String changeLetterToUnderscores() {
+
+        String movieWithDots = "";
+
+        for (int i = 0; i < movie.length(); i++) {
+            char letter = movie.charAt(i);
+
+
+//            if (letter != ' ') {
+//                letter = '_';
+//                movieWithDots += letter;
+//            } else {
+//                movieWithDots += letter;
+//                continue;
+//            }
+            if (letter == ' ') {
+                movieWithDots += " ";
+                continue;
+            }
+            if (goodLetter.indexOf(letter) == -1) {
+                movieWithDots += '_';
+            } else {
+                movieWithDots += letter;
+            }
+        }
+        //System.out.println(movieWithDots);
+        return movieWithDots;
+    }
+
+
 }
--- src/com/company/GuessMovieRun.java
+++ src/com/company/GuessMovieRun.java
@@ -2,37 +2,36 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class GuessMovieRun {
 
     public static void main(String[] args) throws Exception {
 
-        chooseOneFilm();
-
+        String movie = chooseOneFilm();
+        Game game = new Game(movie);
+        game.StartGame(10);
     }
 
-        private static String chooseOneFilm() throws Exception{
+    private static String chooseOneFilm() throws Exception {
 
-            File file = new File("movies.txt");
-            String lists = "";
-            String[] movieLists = {};
+        File file = new File("movies.txt");
+        String lists = "";
+        String[] movieLists = {};
 
-            try{
-                Scanner scanner = new Scanner(file);
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine() + "\n";
-                    lists += line + "\n";
-                    movieLists = lists.split("\n");
-                }
-
-            }catch (FileNotFoundException e){
-                System.out.println("File doesn't found!");
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine() + "\n";
+                lists += line ;
+                movieLists = lists.split("\n");
             }
-            int randomMovie = (int)(Math.random() * movieLists.length);
-            //System.out.println(movieLists[randomMovie]);
-            return movieLists[randomMovie];
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File doesn't found!");
         }
+        int randomMovie = (int) (Math.random() * movieLists.length);
+        //System.out.println(movieLists[randomMovie]);
+        return movieLists[randomMovie];
+    }
 }
